0e64d20d1bfc957daba08e54077f8c49cbf486b0
nanto-hub <n14104@gmail.com>
2026-05-20 13:30:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyggLg4qvPSIvnEl_uNH2EcLTADcB48S9Fpwx1w8Qwq4SoVwSZJR0T-HCQ1veWeBrR8gQ/exec"; 
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbxWvQVFLndSQPgwEISwwmI4KvT4Jza0qVsQ5P6WKIVmDNy-BOxojncn_5tTCNU3MeZKFQ/exec"; 
 const LIFF_ID = "2010122368-FLjBLYl5";
