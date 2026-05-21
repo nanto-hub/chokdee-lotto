add8a802d6761402a6698c700ffe598376fcac80
nanto-hub <n14104@gmail.com>
2026-05-21 10:14:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbxWvQVFLndSQPgwEISwwmI4KvT4Jza0qVsQ5P6WKIVmDNy-BOxojncn_5tTCNU3MeZKFQ/exec"; 
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbz49PNTwYuLrlOTBYyvpo_UFi-AfFgsz3MqKMRP2pN6fm2LloYuit7y6h5XNrq_FJolPQ/exec"; 
 const LIFF_ID = "2010122368-FLjBLYl5";
 const SECRET_TOKEN = "ChokdeeLotto_Khonkaen_2026";
