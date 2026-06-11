dc48e2da99c370194257cf974df8db150904b8ab
nanto-hub <n14104@gmail.com>
2026-06-11 16:39:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbw2QIFpRo8Tygj_Q9-i16eoa6VvjVBsX5Tiag5QbMDRZy1WKOzXjkBBaQumQMt5K5YfQg/exec"; 
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbwl97OhCnvJbZtiOhF6Vz2G9NaCHICrPcBUWi6JZ7eW_Tg-u_xm85JgjnuFKZ1dsN1I9g/exec"; 
 const LIFF_ID = "2010122368-FLjBLYl5";
 const SECRET_TOKEN = "ChokdeeLotto_Khonkaen_2026";
